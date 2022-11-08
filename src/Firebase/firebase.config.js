07a572ee16103a87571d16dd203895cbc220d33c
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-11-09 00:05:37 +0600

    Firebase and tailwind setUp

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyChKWSEUP871z3ciCUlepBUIiUoKLKV1_A",
+    authDomain: "tax-advisor-9c59d.firebaseapp.com",
+    projectId: "tax-advisor-9c59d",
+    storageBucket: "tax-advisor-9c59d.appspot.com",
+    messagingSenderId: "751205416553",
+    appId: "1:751205416553:web:818162d44d54eb6d4cacf8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
