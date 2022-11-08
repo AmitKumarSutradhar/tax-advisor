1c843dce9c6aecc70d0e3af57ad0b4d4cd247fb2
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-11-09 00:54:25 +0600

    Header Section Basic Structure Build Up

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Carousel from '../../../components/Carosual/Carousel';
+import NavMenu from '../../../components/Navbar/NavMenu';
 
 const Header = () => {
     return (
         <div>
-            <h2>Hello from header.</h2>
+            <NavMenu></NavMenu>
+            <Carousel></Carousel>
         </div>
     );
 };
