e523829b903e7da9a251ec51e719ed940621f392
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-11-09 16:06:32 +0600

    react-photo-view setup for serveice image full screen view

--- src/Pages/Services/ServiceBox.js
+++ src/Pages/Services/ServiceBox.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import 'react-photo-view/dist/react-photo-view.css';
-// import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const ServiceBox = ({ service }) => {
     const { _id, img, price, title } = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Shoes" /></figure>
+            <PhotoProvider>
+                <PhotoView src={img}>
+                    <img src={img} alt="" />
+                </PhotoView>
+            </PhotoProvider>
+            {/* <figure><img src={img} alt="Shoes" /></figure> */}
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
--- src/Pages/Services/ServiceDetails.js/ServiceDetail.js
+++ src/Pages/Services/ServiceDetails.js/ServiceDetail.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const ServiceDetail = () => {
     const { _id, img, price, title } = useLoaderData();
@@ -7,7 +9,10 @@ const ServiceDetail = () => {
         <div>
             <h2>Part 1</h2>
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src={img} alt="Shoes" /></figure>
+                <PhotoView src={img}>
+                    <img src={img} style={{ objectFit: 'cover' }} alt="" />
+                </PhotoView>
+                {/* <figure><img src={img} alt="Shoes" /></figure> */}
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
                     <p className='text-2xl text-orange-600 font-semibold'>Price: ${price}</p>
