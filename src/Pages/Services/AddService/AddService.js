fe5bb896a156f9a8abbee4a9c12eee445bf31378
Amit Kumar Sutradhar <amit596450@gmail.com>
2022-11-09 23:49:12 +0600

    Toast for add Service

--- src/Pages/Services/AddService/AddService.js
+++ src/Pages/Services/AddService/AddService.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 const AddService = () => {
     const [service, setServices] = useState({});
 
-    const handleAddService = event => {
+
+
+    const handleAddService = (event) => {
         event.preventDefault();
-        console.log(service);
 
         fetch('http://localhost:5000/services', {
             method: 'POST',
@@ -17,7 +19,6 @@ const AddService = () => {
             .then(res => res.json())
             .then(data => {
                 if (data.acknowledged) {
-                    alert('Services added successfully');
                     event.target.reset();
                 }
             })
@@ -30,42 +31,54 @@ const AddService = () => {
         newService[field] = value;
         setServices(newService);
     }
+
+    const notify = () => {
+        if (service) {
+            toast('Service Added!!!');
+        } else {
+            toast('Please try again');
+        }
+    }
+
+
+
     return (
         <div>
             <h2>Add New Service</h2>
 
-            <form onSubmit={handleAddService} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+            <form onSubmit={handleAddService} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
 
-                <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="text">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="text">
                         Service Title:
                     </label>
                     <input onBlur={handleInputBlur} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name='title' placeholder='name' required />
                     <br />
 
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="text">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="text">
                         Image URL:
                     </label>
                     <input onBlur={handleInputBlur} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name='img' placeholder='Enter Image URL' required />
                     <br />
 
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="price">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="price">
                         Price:
                     </label>
                     <input onBlur={handleInputBlur} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" name='price' placeholder='Enter Price' required />
                     <br />
 
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
                         Servic Details:
                     </label>
                     <input onBlur={handleInputBlur} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" name='description' placeholder='details' required />
+                    <Toaster />
                     <br />
                 </div>
 
 
 
-                <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                <div className="flex items-center justify-between" >
+                    <button onClick={notify} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" >
                         Submit Service
                     </button>
                 </div>
